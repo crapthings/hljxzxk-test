37ff12bfaad7784ca1d88c5593568e821d1fc632
crapthings <crapthings@gmail.com>
2018-11-12 17:54:26 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 const get = require('lodash/get')
+const map = require('lodash/map')
 const express = require('express')
+const ip = require('ip')
+
+let ipaddr = ip.address()
 
 const db = JSON.parse(fs.readFileSync('./db.json', { encoding: 'utf-8' }))
 
@@ -10,6 +14,14 @@ app.get('/', function (req, res) {
   const { code } = req.query
   const result = get(db, code)
   if (!result) return res.sendStatus(500)
+  res.send(result)
+})
+
+app.get('/list', function (req, res) {
+  const result = map(db, (v, k) => ({
+    职权名称: v['职权名称'],
+    连接: `http://${ipaddr}:8910/?code=${k}`,
+  }))
   res.send(result)
 })
 
